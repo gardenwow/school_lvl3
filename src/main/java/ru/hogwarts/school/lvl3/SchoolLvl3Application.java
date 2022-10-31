a4ba3d452adac7170bc4557c8c169cbdeb1d7391
dreamworldsn <107345428+gardenwow@users.noreply.github.com>
2022-10-31 21:10:33 +0400

    new

---
+++ src/main/java/ru/hogwarts/school/lvl3/SchoolLvl3Application.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school.lvl3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolLvl3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolLvl3Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/hogwarts/school/lvl3/SchoolLvl3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school.lvl3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolLvl3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
