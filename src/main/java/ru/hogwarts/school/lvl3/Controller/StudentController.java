8b817ef1ec7dbfe107cbdda434c54c62f4ba401a
dreamworldsn <107345428+gardenwow@users.noreply.github.com>
2022-11-13 17:50:31 +0400

    database

--- src/main/java/ru/hogwarts/school/lvl3/Controller/StudentController.java
+++ src/main/java/ru/hogwarts/school/lvl3/Controller/StudentController.java
@@ -1,13 +1,19 @@
 package ru.hogwarts.school.lvl3.Controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.hogwarts.school.lvl3.Model.Faculty;
+import org.springframework.web.multipart.MultipartFile;
+import ru.hogwarts.school.lvl3.Model.Avatar;
 import ru.hogwarts.school.lvl3.Model.Student;
 import ru.hogwarts.school.lvl3.Service.StudentService;
 
-import java.util.Collection;
-import java.util.Collections;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @RequestMapping("/student")
 @RestController
@@ -21,7 +27,7 @@ public class StudentController {
 
     @GetMapping("{id}")
     public ResponseEntity<Student> getStudent(@PathVariable long id){
-        Student student = studentService.readStudent(id);
+        Student student = studentService.findStudent(id);
         if (student == null){
             return ResponseEntity.badRequest().build();
         }
@@ -34,18 +40,33 @@ public class StudentController {
     }
     @PostMapping
     public ResponseEntity<Student> createStudent(@RequestBody Student student){
-        Student createStudent = studentService.createStudent(student);
+        Student createStudent = studentService.addStudent(student);
         return ResponseEntity.ok(createStudent);
     }
     @PutMapping()
     public ResponseEntity<Student> updateStudent(@RequestBody Student student){
-        Student updateStudent = studentService.updateStudent(student.getId(), student);
+        Student updateStudent = studentService.editStudent(student);
         return ResponseEntity.ok(updateStudent);
     }
     @DeleteMapping("{id}")
     public void deleteStudent(@PathVariable long id){
          studentService.deleteStudent(id);
     }
+    @GetMapping(value = "/{id}/avatar")
+    public void downloadAvatar(@PathVariable Long id, HttpServletResponse response) throws IOException {
+        Avatar avatar = studentService.findAvatar(id);
+
+        Path path = Path.of(avatar.getFilePath());
+
+        try (InputStream is = Files.newInputStream(path);
+             OutputStream os = response.getOutputStream();) {
+            response.setStatus(200);
+            response.setContentType(avatar.getMediaType());
+            response.setContentLength((int) avatar.getFileSize());
+            is.transferTo(os);
+        }
+    }
+
    /*
     @GetMapping
     public ResponseEntity<Collection<Student>> findColor(@RequestParam(required = false) int age){
--- src/main/java/ru/hogwarts/school/lvl3/Service/StudentService.java
+++ src/main/java/ru/hogwarts/school/lvl3/Service/StudentService.java
@@ -1,56 +1,68 @@
 package ru.hogwarts.school.lvl3.Service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import ru.hogwarts.school.lvl3.Model.Faculty;
-import ru.hogwarts.school.lvl3.Model.Student;
-import ru.hogwarts.school.lvl3.repositories.StudentRepository;
+import static java.nio.file.StandardOpenOption.CREATE_NEW;
 
-
-import java.util.ArrayList;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Collection;
-import java.util.HashMap;
+import javax.imageio.ImageIO;
+import javax.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import ru.hogwarts.school.lvl3.Model.Avatar;
+import ru.hogwarts.school.lvl3.Model.Student;
+import ru.hogwarts.school.lvl3.repositories.AvatarRepository;
+import ru.hogwarts.school.lvl3.repositories.StudentRepository;
 
 @Service
 public class StudentService {
-    private final StudentRepository studentRepository;
 
-    public StudentService(StudentRepository studentRepository) {
+    private final StudentRepository studentRepository;
+    private final AvatarRepository avatarRepository;
+
+    public StudentService(StudentRepository studentRepository, AvatarRepository avatarRepository) {
         this.studentRepository = studentRepository;
+        this.avatarRepository = avatarRepository;
     }
 
-
-    public Student createStudent(Student student) {
+    public Student addStudent(Student student) {
+        student.setId(null);
         return studentRepository.save(student);
     }
 
-    public Student readStudent(long id) {
-        return studentRepository.findById(id).get();
+    public Student findStudent(long id) {
+        return studentRepository.findById(id).orElseThrow();
     }
 
-    public Student updateStudent(long id, Student student) {
+    public Student editStudent(Student student) {
         return studentRepository.save(student);
     }
 
     public void deleteStudent(long id) {
         studentRepository.deleteById(id);
-
     }
 
-    public Collection <Student> findBetweenAge(int a, int b){
+
+    public Avatar findAvatar(long studentId) {
+        return avatarRepository.findByStudentId(studentId).orElseThrow();
+    }
+
+    private String getExtension(String fileName) {
+        return fileName.substring(fileName.lastIndexOf(".") + 1);
+    }
+
+
+    public Collection<Student> findBetweenAge(int a, int b) {
         return studentRepository.findByAgeBetween(a, b);
-    }
-    /*
-    public Collection<Student> findAge(int age) {
-        ArrayList<Student> students = new ArrayList<>();
-        for (Student student : studentHashMap.values()) {
-            if (student.getAge() == age) {
-                students.add(student);
-            }
 
-        }
-        return students;
     }
-
-     */
 }
